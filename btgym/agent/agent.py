94b4707e0655a66a297ca23195a6f9d266a7a4d7
ChenXL97 <908926798@qq.com>
2024-03-25 22:25:01 +0800

    update

--- btgym/agent/agent.py
+++ btgym/agent/agent.py
@@ -1,10 +1,36 @@
 
-
 class Agent(object):
+    env = None
+    response_frequency = 1
+
     def __init__(self):
         self.condition_set = set()
+        self.init_statistics()
 
     def bind_bt(self,bt):
         self.bt = bt
         bt.bind_agent(self)
 
+    def init_statistics(self):
+        self.step_num = 1
+        self.next_response_time = self.response_frequency
+        self.last_tick_output = None
+
+    def step(self):
+        if self.env.time > self.next_response_time:
+            self.next_response_time += self.response_frequency
+            self.step_num += 1
+
+            self.bt.tick()
+            bt_output = self.bt.visitor.output_str
+
+            if bt_output != self.last_tick_output:
+                print(f"==== time:{self.env.time:f}s ======")
+
+                print(bt_output)
+
+                print("\n")
+                self.last_tick_output = bt_output
+                return True
+            else:
+                return False

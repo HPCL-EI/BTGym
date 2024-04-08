89ab214e149591c6da2a2394ed0e04abac6ef827
ChenXL97 <908926798@qq.com>
2024-04-08 10:11:07 +0800

    更新api

---
+++ btgym/algos/llm_client/llms/gpt4.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+
+
+
+class LLMGPT4():
+    def __init__(self):
+        self.client = OpenAI(
+            base_url="https://kapkey.chatgptapi.org.cn/v1",            api_key="sk-YX6Kn2rtcXHmz4en9f6c2931Bf194eD892A09e70E7493fA9"
+        )
+
+    def request(self,question):
+        completion = self.client.chat.completions.create(
+          model="gpt-4-turbo-preview",
+          messages=[
+            {"role": "system", "content": ""},#You are a helpful assistant.
+            {"role": "user", "content": question}
+          ]
+        )
+
+        return completion.choices[0].message.content
+
+
+if __name__ == '__main__':
+    llm = LLMGPT4()
+    answer = llm.request(question="are you gpt4?")
+    print(answer)

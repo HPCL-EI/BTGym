2fb3a306dca63036393b81f1880c421ef798ef1d
Caiyishuai <39987654+Caiyishuai@users.noreply.github.com>
2024-06-04 21:06:18 +0800

    update

--- z_benchmark/离目标的距离_plot.py
+++ z_benchmark/离目标的距离_plot.py
@@ -10,13 +10,19 @@ def plot_percentage(percentages_type, difficulty, scene, algo_type, max_epoch):
     mean_corr_ratio = []  # 存储5个算法下的mean
     std_corr_ratio = []  # 存储5个算法下的std
     for algo_str in algo_type:
-        file_path = f'./output_percentage/{percentages_type}_{difficulty}_{scene}_{algo_str}.csv'
+        file_path = f'./percentage_output/{percentages_type}_{difficulty}_{scene}_{algo_str}.csv'
         df = pd.read_csv(file_path)
 
         corr_ratio_all = np.array(df)   # 每个data的所有epoch的ratio
+        print(algo_str,len((list(np.mean(corr_ratio_all, axis=0)))))
         mean_corr_ratio.append(list(np.mean(corr_ratio_all, axis=0)))  # epoch的平均mean
         std_corr_ratio.append(list(np.std(corr_ratio_all, axis=0)))  # epoch的平均std
 
+    # print(algo_str)
+    # print(corr_ratio_all.shape)
+    # print(len(mean_corr_ratio))
+    # print(len(mean_corr_ratio[0]))
+    # print(std_corr_ratio.shape,'\n')
     mean_corr_ratio = np.array(mean_corr_ratio)
     std_corr_ratio = np.array(std_corr_ratio)
     epochs = np.arange(1, max_epoch + 1)
@@ -35,16 +41,16 @@ def plot_percentage(percentages_type, difficulty, scene, algo_type, max_epoch):
     plt.title(f'{percentages_type} ratio in {scene} ({difficulty})')
     plt.legend()
     plt.grid(True)
-    plt.savefig(f'./images_percentage/{percentages_type}_{difficulty}_{scene}.png', dpi=100)
+    plt.savefig(f'./percentage_images/{percentages_type}_{difficulty}_{scene}.png', dpi=100)
     plt.show()
 
-max_epoch = 1000
-algo_type = ['opt_h0','obtea']   # 'opt_h0','opt_h0_llm', 'obtea', 'bfs',      'opt_h1','weak'
+max_epoch = 2000
+algo_type = ['opt_h0', 'obtea', 'bfs']   # 'opt_h0','opt_h0_llm', 'obtea', 'bfs',      'opt_h1','weak'
 
 for percentages_type in ['expanded']:  # 'expanded', 'traversed', 'cost'
-    for difficulty in ['single']:  # 'single', 'multi'
+    for difficulty in ['single', 'multi']:  # 'single', 'multi'
         print(f"============ percentages_type = {percentages_type}, difficulty = {difficulty} =============")
-        for scene in ['VH']:  # 'RH', 'RHS', 'RW', 'VH'
+        for scene in ['RH', 'RHS', 'RW', 'VH']:  # 'RH', 'RHS', 'RW', 'VH'
             print(f"++++++++++ scene = {scene} ++++++++++")
             plot_percentage(percentages_type, difficulty, scene, algo_type, max_epoch)
 
